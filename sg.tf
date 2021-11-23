4a078e6811e6a7c6a239112e036949414fbfa544
Chetan Talwar <thechetantalwar@users.noreply.github.com>
2021-11-23 11:29:26 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "mysg"{
+    name = "Custom Security Group"
+    ingress {
+    description = "SSH ACCESS"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+        egress {
+        description = "All Traffic"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+}
